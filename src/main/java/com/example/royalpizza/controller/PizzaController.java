7cb91f35a9367752563b3a8c75448e540d90a78e
Hassen-GHAZEL <hassen0805@gmail.com>
2025-09-23 02:34:49 +0200

    correction => post -> get et ajout du Autowired

--- src/main/java/com/example/royalpizza/controller/PizzaController.java
+++ src/main/java/com/example/royalpizza/controller/PizzaController.java
@@ -2,6 +2,7 @@ package com.example.royalpizza.controller;
 
 import com.example.royalpizza.entity.Pizza;
 import com.example.royalpizza.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,9 +11,10 @@ import java.util.List;
 @RequestMapping("/pizzas")
 public class PizzaController {
 
+    @Autowired
     private PizzaService pizzaService;
 
-    @PostMapping
+    @GetMapping
     public List<Pizza> getAllPizzas() {
         return pizzaService.getAllPizzas();
     }
